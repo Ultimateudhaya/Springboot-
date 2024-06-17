1d42c6c5aa6bedded5095184884b1e964cbe00b0
Udhayakumar <72443950+Ultimateudhaya@users.noreply.github.com>
2024-06-17 20:21:55 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // Getters and setters
+    // Constructors
+}
